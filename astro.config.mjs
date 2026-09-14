c2c98cd36dd362147b4b8f314452169622f94102
Martial Lopez <martial.lopez@protonmail.com>
2026-09-15 00:24:29 +0200

    SOSQLIK V4.57 - articles DELTA MERGE et nettoyage tutoriels

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   site: 'https://sosqlik.fr',
-  output: 'static'
+  output: 'static',
+  adapter: cloudflare()
 });
